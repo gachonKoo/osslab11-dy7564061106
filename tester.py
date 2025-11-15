5f3645699ecc257bff853af65f566030a5ebb081
dy7564061106 <dy7564061106@gmail.com>
2025-11-15 15:53:08 +0900

    submit dy7564061106

---
+++ tester.py
@@ -0,0 +1,10 @@
+from geo.utils import add, sub, mul, div
+
+def main():
+    print(add(2, 3))
+    print(sub(5, 2))
+    print(mul(2, 4))
+    print(div(10, 2))
+
+if __name__ == '__main__':
+    main()
